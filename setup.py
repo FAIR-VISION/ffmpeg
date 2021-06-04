593b895764c1a2c9a2ec07c75b61f7ea6ba1d806
JeanLecigne <j.lecigne@fairvision.fr>
2021-06-04 13:10:08 +0000

    Add new file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='ffmpeg',
+    version='0.1',
+    packages=[''],
+    url='None',
+    license='None',
+    author='ExterneSTAGIAIRE1',
+    author_email='None',
+    description='None'
+)
